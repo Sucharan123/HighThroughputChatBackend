8164f7834f38ffa264d11d51e0ab580fb44970b2
THIPPARAVENI SUCHARAN <sucharanthipparaveni4@gmail.com>
2026-07-04 16:34:01 +0530

    Initial SpringBoot setup

---
+++ src/main/java/com/example/Htcb/HtcbApplication.java
@@ -0,0 +1,13 @@
+package com.example.Htcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtcbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtcbApplication.class, args);
+	}
+
+}
